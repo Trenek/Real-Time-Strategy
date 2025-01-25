64a1dfc9c4490dace59eb4a6e55d5cfdc2ef8362
AgnSud <ag.suder@student.uj.edu.pl>
2025-01-25 14:15:48 +0100

    Update of tests for cohords and test_main

--- tests/test_main.c
+++ tests/test_main.c
@@ -6,9 +6,6 @@ void test_checkForClick(void);
 void test_setPosition(void);
 void test_highlightCohort(void);
 
-void test_warriors(void);
-void test_enemy(void);
-
 void test_armyDisplacement(void);
 
 void test_warriorMovement(void);
@@ -17,6 +14,11 @@ void test_barbarianMovement(void);
 void test_giveAcceleration(void);
 void test_calculateVelocity(void);
 void test_move(void);
+
+void test_attackCohord_noEnemies(void);
+void test_attackCohord_withEnemies(void);
+void test_attackCohord_healthUpdates(void);
+void test_attackCohord_removeDeadUnits(void);
 
 
 int main(void) {
@@ -27,32 +29,33 @@ int main(void) {
     RUN_TEST(test_checkForClick);
 
     // test cohord
-//    RUN_TEST(test_setPosition); - nie uruchamia się narazie, ale nie wiem czemu
+    RUN_TEST(test_setPosition);
     RUN_TEST(test_highlightCohort);
 
-    // test cohordModels
-//    RUN_TEST(test_warriors); - nie uruchamia się narazie, ale nie wiem czemu
-//    RUN_TEST(test_enemy); - nie uruchamia się narazie, ale nie wiem czemu
-
     // test displacement
-    RUN_TEST(test_armyDisplacement); // - fail, przeliczyć
+    RUN_TEST(test_armyDisplacement);
 
     // test fightBuilder
     RUN_TEST(test_warriorMovement);
-    RUN_TEST(test_barbarianMovement);  // - fail, przeliczyć
+    RUN_TEST(test_barbarianMovement);
 
     // test movement
-    RUN_TEST(test_giveAcceleration); // - fail, przeliczyć
+    RUN_TEST(test_giveAcceleration);
     RUN_TEST(test_calculateVelocity);
     RUN_TEST(test_move);
 
+    //test fight
+    RUN_TEST(test_attackCohord_noEnemies); // - fail, przeliczyć - nie wiem dlaczego jest fail dlaczego ten ostatni sie nie inicjalizuje na 0
+    RUN_TEST(test_attackCohord_withEnemies); // - fail, przeliczyć
+    RUN_TEST(test_attackCohord_healthUpdates); // - fail, przeliczyć
+    RUN_TEST(test_attackCohord_removeDeadUnits);
 
 
     // Informacje końcowe o liczbie testów
-    printf("\n[INFO] Wszystkie testy zostały wykonane.\n");
-    printf("[INFO] Liczba testów: %llu\n", Unity.NumberOfTests);
-    printf("[INFO] Liczba zakończonych błędem: %llu\n", Unity.TestFailures);
-    printf("[INFO] Liczba zakończonych powodzeniem: %llu\n", Unity.NumberOfTests - Unity.TestFailures);
+    printf("\n[INFO] Wszystkie testy zostaly wykonane.\n");
+    printf("[INFO] Liczba testow: %llu\n", Unity.NumberOfTests);
+    printf("[INFO] Liczba zakonczonych bledem: %llu\n", Unity.TestFailures);
+    printf("[INFO] Liczba zakonczonych powodzeniem: %llu\n", Unity.NumberOfTests - Unity.TestFailures);
 
     return UNITY_END();
 }
